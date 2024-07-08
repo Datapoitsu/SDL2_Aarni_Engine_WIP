e179db44662100ba3ff524d792953d1cce1953cb
Aarni Junkkala <aarni.junkkala@gmail.com>
2024-07-08 14:16:16 +0300

    Updata

--- main.cpp
+++ main.cpp
@@ -3,20 +3,24 @@
 #include <SDL2/SDL_image.h>
 #include <math.h>
 #include <time.h>
+#include <windows.h>
+
 #define PI 3.141592654
 
 const int WIDTH = 400, HEIGHT = 400;
+
 SDL_Texture *img = NULL;
 int w, h; // texture width & height
+
 float dist = 100;
-float timer = 0;
-float total = 0;
-double cpu_time_used;
-int Mspf = 0;
+float playerRot;
+
+// Time calculation
 timeval t1, t2;
 double elapsedTime;
-double delta;
-double Timer2 = 0;
+double deltaTime;
+int fpsLimiter = 60;
+
 int main( int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -31,18 +35,15 @@ int main( int argc, char *argv[])
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
     img = IMG_LoadTexture(renderer, "src/Game/Sprites/hahmo.png");
-	SDL_QueryTexture(img, NULL, NULL, &w, &h); // get the width and height of the texture
-	// put the location where we want the texture to be drawn into a rectangle
-	// I'm also scaling the texture 2x simply by setting the width and height
+	SDL_QueryTexture(img, NULL, NULL, &w, &h);
 	
-
     SDL_Event windowEvent;
-
-    mingw_gettimeofday(&t1, NULL);
+    
+    //Begining of calculating time.
+    mingw_gettimeofday(&t1, NULL); 
 
     while (true)
     {
-        
         if (SDL_PollEvent(&windowEvent))
         {
             if (SDL_QUIT == windowEvent.type)
@@ -51,31 +52,22 @@ int main( int argc, char *argv[])
             }
         }
 
-        for(int i = 0; i < 1000*10000; i++){
-            int a = i;
-            int b = i / 2;
-            int c = a * b;
-        }
+        playerRot += deltaTime * PI * 2;
 
-        timer += delta * PI * 2;
-
-        SDL_Rect texr; texr.x = cos(timer) * dist + (WIDTH / 2); texr.y = sin(timer) * dist + (HEIGHT / 2); texr.w = w; texr.h = h; 
+        SDL_Rect texr; texr.x = cos(playerRot) * dist + (WIDTH / 2); texr.y = sin(playerRot) * dist + (HEIGHT / 2); texr.w = w; texr.h = h; 
         SDL_RenderClear(renderer); 
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderCopy(renderer, img, NULL, &texr);
         SDL_RenderPresent(renderer);
 
+        //fps limiter
+        Sleep(std::max(0.0,(1000 / fpsLimiter) - (deltaTime * 1000)));
+        
+        //Calculating passing time.
         mingw_gettimeofday(&t2, NULL);
-
-        //compute
         elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;
         elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;
-
-        //convert to seconds
-        delta = elapsedTime /1000;
-        Timer2 += delta;
-        std::cout << Timer2 << std::endl;
-
+        deltaTime = elapsedTime / 1000;
         t1 = t2;
     }
 
